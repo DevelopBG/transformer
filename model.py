42f07cf78cd0d5b5dfa042e2683c58c25914f7ef
DevelopBG <bani.ghosh10@gmail.com>
2025-06-01 23:13:02 +1000

    formation of input embedding

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
 import torch
 import torch.nn as nn
+import math
 
 class InputEmbeddings(nn.Module):
-    
+    def __init__(self, d_model, vocab_size):
+        super.__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(num_embeddings=self.vocab_size,embedding_dim=self.d_model)
+    def forward(self,x):
+        return self.embedding(x)*math.sqrt(self.d_model)
