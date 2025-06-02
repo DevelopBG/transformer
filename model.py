17d7aa7af19c17d425958634e7b1f713187e1f12
DevelopBG <bani.ghosh10@gmail.com>
2025-06-02 12:59:16 +1000

    layer normalization addition

--- model.py
+++ model.py
@@ -35,4 +35,20 @@ class PostionalEmbedding(nn.Module):
     def forward(self,x):
         x = x + (self.pe[:,:x.shape[1],:]).required_grad(False)
         return self.dropout(x)
+    
+
+# layer norm 
+class LayerNormalization(nn.Module):
+    def __init__(self,eps:float=10**-6):
+        super.__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #multiplied
+        self.beta = nn.Parameter(torch.zeros(1)) # added
+
+    def forward(self,x):
+        mean =  x.mean(dim=-1,keepdim = True)
+        std = x.std(dim=-1,keepdim = True)
+        return self.alpha*(x-mean)/(std+self.eps) +self.beta
+    
+    
 
