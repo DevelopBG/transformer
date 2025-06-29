fce6eb15fa1e4b8214bd5a49dc6bf4ae5e72a39c
DevelopBG <bani.ghosh10@gmail.com>
2025-06-29 13:24:36 +1000

    final dataset

--- dataset.py
+++ dataset.py
@@ -71,7 +71,8 @@ class BilingualDataset(Dataset):
             'encoder_mask' : (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #( 1,1,seq_len)
             'decoder_mask' : (decoder_input  != self.pad_token).unsqueeze(0).unsqueeze(0).int() & causal_mask(decoder_input.size(0)), # (1,seq_len) & (1, seq_len, seq_len)
             'label' : label, # seq_len
-            'scr_text' : src_text
+            'scr_text' : src_text,
+            'tgt_text' : tgt_text
         }
     
 def causal_mask(size):
